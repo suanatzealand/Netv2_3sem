b2b9e9b16af5c5e60d1853f2bfc51d82c27b0641
Sune <suan@zealand.dk>
2026-09-04 14:33:46 +0200

    firefox

--- hello_playwright.py
+++ hello_playwright.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
- for browser_type in [p.chromium]:
+ for browser_type in [p.firefox]:
         browser = browser_type.launch()
         page = browser.new_page()
         page.goto('https://zealand.dk')
